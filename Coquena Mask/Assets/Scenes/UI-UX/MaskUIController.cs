acc03ac145bdc83ee4a67255c4404c316bdf1b3f
JoseMarquez <jlm270901@gmail.com>
2026-02-04 02:57:47 -0300

    Script

---
+++ Coquena Mask/Assets/Scenes/UI-UX/MaskUIController.cs
@@ -0,0 +1,39 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaskUIController : MonoBehaviour
+{
+    public Image maskIcon;
+    public Image glow;
+    UIAnimationHelper anim;
+
+    void Awake()
+    {
+        anim = GetComponent<UIAnimationHelper>();
+    }
+
+    void OnEnable()
+    {
+        PlayerScript.OnMaskChanged += HandleMaskChanged;
+    }
+
+    void OnDisable()
+    {
+        PlayerScript.OnMaskChanged -= HandleMaskChanged;
+    }
+
+    void HandleMaskChanged(Mask newMask)
+    {
+        // Cambiar icono
+        maskIcon.sprite = newMask.icon;
+
+        // Color según tipo
+        Color typeColor = newMask.maskColor;
+        glow.color = new Color(typeColor.r, typeColor.g, typeColor.b, 0.4f);
+
+        // 🎬 Animación por código
+        anim.AnimateScale(Vector3.one * 0.8f, Vector3.one, 0.15f);
+        anim.AnimateColor(glow, Color.clear, glow.color, 0.2f);
+    }
+}
